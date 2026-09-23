31f81d17d7510c50b752890b54b2a1090b86851e
kepr <kempedron@gmail.com>
2026-09-24 02:56:51 +0300

    temp: switch base to /whalli and remove CNAME for preview on kempedron.github.io

--- whalli-book/astro.config.mjs
+++ whalli-book/astro.config.mjs
@@ -9,8 +9,8 @@ const whalliGrammar = JSON.parse(
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://whalli.is-a.dev',
-	base: '/',
+	site: 'https://kempedron.github.io',
+	base: '/whalli',
 	integrations: [
 		starlight({
 			title: 'Whalli',
